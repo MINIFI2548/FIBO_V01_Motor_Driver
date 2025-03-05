4ab38ef1cba789860282b14c94beffcea9bcffa5
MINIFI2548 <Sarayuth2548@gmail.com>
2025-03-05 19:19:00 +0700

    basic requirement
    control dutycycle
    read encoder
    have a gear ratio between encoder and output
    can set ppr

--- src/FIBO_V01_Motor_Driver.cpp
+++ src/FIBO_V01_Motor_Driver.cpp
@@ -1,2 +1,74 @@
 #include<Arduino.h>
 #include"FIBO_V01_Motor_Driver.h"
+#include <pio_encoder.h>
+
+// set up
+Motor :: Motor(uint8_t ENB, uint8_t HIGH_PIN, uint8_t LOW_PIN, uint8_t ENCODER_A, uint8_t ENCODER_B, uint8_t HOMING) 
+    : encoder(ENCODER_A), ENB(ENB), HIGH_PIN(HIGH_PIN), LOW_PIN(LOW_PIN), HOMING(HOMING){
+}   
+
+void Motor :: info() { 
+    Serial.print("ENB PIN -> ");
+    Serial.print(ENB);
+    Serial.print(" | HIGHT PIN -> ");
+    Serial.print(HIGH_PIN);
+    Serial.print(" | LOW PIN -> ");
+    Serial.println(LOW_PIN);
+    Serial.print("EncoderA -> ");
+    Serial.print(ENC_A);
+    Serial.print(" | EncoderB -> ");
+    Serial.println(ENC_B);
+    Serial.print("Pulses Per Revolution -> ");
+    Serial.println(pulsesPerRevolution);
+    Serial.print("Ratio between Encoder & Output -> ");
+    Serial.println(gearRatio);
+}
+
+void Motor :: begin() {
+    pinMode(ENB, OUTPUT);
+    pinMode(HIGH_PIN, OUTPUT);
+    pinMode(LOW_PIN, OUTPUT);
+    pinMode(HOMING, INPUT_PULLUP);
+    encoder.begin();
+    // pinMode(ENC_A, INPUT_PULLUP);
+    // pinMode(ENC_B, INPUT_PULLUP);
+}
+
+void Motor :: setDutycycle(int dutycycle) {
+    if(dutycycle > 0){ 
+        digitalWrite(ENB, LOW);
+        analogWrite(HIGH_PIN, dutycycle);
+        digitalWrite(LOW_PIN, LOW);
+    } 
+    else if(dutycycle < 0){ 
+        digitalWrite(ENB, LOW);
+        digitalWrite(HIGH_PIN, LOW);
+        analogWrite(LOW_PIN, -dutycycle);
+    }else{
+        digitalWrite(ENB, HIGH);
+        digitalWrite(HIGH_PIN, LOW);
+        digitalWrite(LOW_PIN, LOW);
+    }
+    this->dutycycle=dutycycle;
+
+}
+void Motor :: setGearRatio(double gearRatio){
+    this -> gearRatio = gearRatio;
+}
+void Motor :: setPPR(int pulsesPerRevolution){
+    this -> pulsesPerRevolution = pulsesPerRevolution;
+}
+
+bool Motor :: homed(){
+    return digitalRead(HOMING);
+}
+long Motor :: getEncoder(){ 
+    return encoder.getCount();
+}
+double Motor :: getRevolution(){
+    return double(encoder.getCount()) / double(pulsesPerRevolution) * gearRatio;
+}
+
+int Motor ::getDutycycle(){
+    return dutycycle;
+}
--- src/FIBO_V01_Motor_Driver.h
+++ src/FIBO_V01_Motor_Driver.h
@@ -1,1 +1,27 @@
 #include<Arduino.h>
+#include "pio_encoder.h"
+
+class Motor{
+    private :
+        uint8_t ENB, HIGH_PIN, LOW_PIN, ENC_A, ENC_B, HOMING;
+        int pulsesPerRevolution = 8192;
+        double gearRatio = 1; // encoder / output
+       
+        PioEncoder encoder;
+        int dutycycle;
+     public :
+        Motor(uint8_t ENB, uint8_t HIGH_PIN, uint8_t LOW_PIN, uint8_t ENCODER_A, uint8_t ENCODER_B, uint8_t HOMING);
+
+        // set & set function
+        void begin();
+        void info();
+        
+        bool homed();
+        long getEncoder(); // get valuable for motor 
+        double getRevolution(); // get revolution of output
+        void setGearRatio(double gearRatio);
+        void setPPR(int pulsesPerRevolution);
+        int getDutycycle();
+        // controll function 
+        void setDutycycle(int dutycycle);     
+};
